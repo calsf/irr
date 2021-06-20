5374ddb775e95c1a78c016d0dbce4ec7bf19fe9d
calsf <lefrogjones@gmail.com>
2021-06-20 17:42:49 -0400

    adding rooms, modifying reflect, default proj will destroy if alive too long

--- enemies/base_dmg_player/default_projectile.gd
+++ enemies/base_dmg_player/default_projectile.gd
@@ -3,12 +3,20 @@
 extends DamagePlayer
 class_name DefaultProjectile
 
+# Destroy automatically in case projectile is stuck alive (i.e reflecting)
+const MAX_TIME_ALIVE = 30
+
 var Explo = preload("res://enemies/base_dmg_player/ProjectileExplo.tscn")
 
 onready var shadow = $Shadow
+onready var timer = $Timer
 
 # Signal emitted before this projectile gets destroyed
 signal destroyed()
+
+func _ready():
+	timer.connect("timeout", self, "_destroy")
+	timer.start(MAX_TIME_ALIVE)
 
 # If hits with anything in layer mask, destroys this projectile
 func _on_hit(other):
@@ -24,6 +32,14 @@ func _on_hit(other):
 	explo.global_position = global_position
 	
 	emit_signal("destroyed")
+	queue_free()
+
+func _destroy():
+		# Create explo effect, effect's shadow inherits this projectile's shadow pos
+	var explo = Explo.instance()
+	get_tree().current_scene.add_child(explo)
+	explo.get_node("Shadow").position = shadow.position
+	explo.global_position = global_position
 	queue_free()
 
 # Transition from start up to loop animation
--- enemies/level6_enemies/mirror/enemy_reflect.gd
+++ enemies/level6_enemies/mirror/enemy_reflect.gd
@@ -13,4 +13,12 @@ func _on_hit(other):
 	# Must be a DamagePlayer object to reflect
 	if other.get_owner() is DamagePlayer and other.get_owner().get_speed() > 0:
 		_atk_anim.play("attack")
-		other.get_owner().dir = (global_position - other.get_owner().dir).normalized()
+		var last_dir = other.get_owner().dir
+		var new_dir = (global_position - other.get_owner().global_position).normalized()
+		
+		# Adjust y direction of projectile if it didn't change
+		# Does not work with projectiles that have a 0 value for y
+		if (sign(new_dir.y) == sign(last_dir.y)):
+			new_dir.y = -new_dir.y
+		
+		other.get_owner().dir = new_dir
